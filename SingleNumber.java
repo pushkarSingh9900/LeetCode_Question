2494b2de80ec819dbcc037bc0c9f4b37ae329c52
pushkarSingh9900 <151795316+pushkarSingh9900@users.noreply.github.com>
2024-03-18 20:57:54 -0400

    Create SingleNumber.java

---
+++ SingleNumber.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int res  = 0;
+        for(int i=0; i<nums.length; i++){
+            map.put(nums[i],map.getOrDefault(nums[i], 0) + 1);
+        }
+        for(int key : map.keySet()){
+            if(map.get(key) == 1){
+                res = key;
+            }
+        }
+        return res;
+    }
+}
